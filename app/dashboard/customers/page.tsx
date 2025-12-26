08acf0907792852edcfe066dd859bfe5f24e5ea6
A-S-droid <ahmadsuhari49@gmail.com>
2025-12-26 20:00:44 +0200

    customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import Table from "@/app/ui/customers/table";
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  return (
+    <div className="w-full">
+      <Table query={query} />
+    </div>
+  );
 }
